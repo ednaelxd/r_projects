e492da8e5e616f8b192963979ed5dc1c4118f6e7
ednaelxd <90271690+ednaelxd@users.noreply.github.com>
2021-10-21 12:06:47 -0300

    Rename anexo1.0.2.R.R to AnalisesdeDadosJU/anexo1.0.2.R.R

--- AnalisesdeDadosJU/anexo1.0.2.R.R
+++ AnalisesdeDadosJU/anexo1.0.2.R.R
@@ -1,11 +1,11 @@
 Avaliador<-function(x,y,z){
-#x é a matriz
-#y é o número de linhas
-#z é o número de colunas
+#x Ã© a matriz
+#y Ã© o nÃºmero de linhas
+#z Ã© o nÃºmero de colunas
 
 media<-matrix #matriz com media de cada linha
 matrizR<-matrix #matriz soma de cada linha
-matrizI<-matrix #matriz com interpretação de cada linha
+matrizI<-matrix #matriz com interpretaÃ§Ã£o de cada linha
 matrizF<-matrix #matriz com dados finais
 
 for(i in 1:y){
@@ -17,23 +17,23 @@ for(i in 1:y){
 media[i,1]= matrizR[i,1]/(length(colnames(x)))
 
 	if(media[i,1]>=3.6 && media[1,i]<=4.4){
-	  matrizI[i,1]<-"Relação ótima"
+	  matrizI[i,1]<-"RelaÃ§Ã£o Ã³tima"
 	}
 	else{
 	if(media[i,1]>=2.7 && media[1,i]<=3.5){
-	  matrizI[i,1]<-"Relação boa"
+	  matrizI[i,1]<-"RelaÃ§Ã£o boa"
 	}
 	else{
 	if(media[i,1]>=1.8 && media[1,i]<=2.6){
-	  matrizI[i,1]<-"Relação regular"
+	  matrizI[i,1]<-"RelaÃ§Ã£o regular"
 	}
 	else{
 	if(media[i,1]>=0.9 && media[1,i]<=1.7){
-	  matrizI[i,1]<-"Relação ruim"
+	  matrizI[i,1]<-"RelaÃ§Ã£o ruim"
 	}
 	else{
 	if(media[i,1]>=0 && media[1,i]<=0.8){
-	  matrizI[i,1]<-"Relação péssima"
+	  matrizI[i,1]<-"RelaÃ§Ã£o pÃ©ssima"
 	 }
 	}
      }
